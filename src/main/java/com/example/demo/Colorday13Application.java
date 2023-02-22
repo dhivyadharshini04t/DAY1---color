bba12319bdbe1bc9c13aeeca4f1c5a0283bae083
dhivyadharshini04t <dhivyadharshini04t@gmail.com>
2023-02-22 15:03:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/Colorday13Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Colorday13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Colorday13Application.class, args);
+	}
+
+}
